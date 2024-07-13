ce5e62cf693974446aba2f5d305c8f160c515caf
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-12 22:57:11 -0400

    client side completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+
 export default function Login() {
+    const [data, setData] = useState({
+        email: "", 
+        password: ""
+    })
+
+    const loginUser = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <>
-            Login
-        </>
+        <div>
+            <form style={{ display: "flex", flexDirection: "column", width: "100%", padding: "30px", margin: "10px" }} onSubmit={handleSubmit} >
+                <label>Email Address</label>
+                <input type="email" placeholder="Email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+
+                <label>Password</label>
+                <input type="password" placeholder="Password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                <button type="submit" style={{ margin: "10px 0px 10px" }}>Submit</button>
+            </form>
+        </div>
     )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+
 export default function Register() {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <>
-            Home
-        </>
+        <div>
+            <form style={{ display: "flex", flexDirection: "column", width: "100%", padding: "30px", margin: "10px" }} onSubmit={handleSubmit} >
+                <label>Name</label>
+                <input type="text" value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} placeholder="Name" />
+
+                <label>Email Address</label>
+                <input type="email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} placeholder="Email" />
+
+                <label>Password</label>
+                <input type="password" placeholder="Password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                <button type="submit" style={{ margin: "10px 0px 10px" }}>Submit</button>
+            </form>
+        </div>
     )
 }
